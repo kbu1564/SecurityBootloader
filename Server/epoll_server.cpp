ac39777d8603a82684ac78c3ce0e086c3a04737b
Byeoung Wook <quddnr145@gmail.com>
2015-07-02 23:49:47 +0900

    네트워크 클래스 내부 주석 추가

--- Server/epoll_server.cpp
+++ Server/epoll_server.cpp
@@ -9,21 +9,33 @@ EpollServer::~EpollServer()
 {
 }
 
+/**
+ * 소켓 생성
+ */
 int EpollServer::open(const int port)
 {
 	return E_OK;
 }
 
+/**
+ * 서버 소켓 닫기
+ */
 int EpollServer::shutdown()
 {
 	return E_OK;
 }
 
+/**
+ * 비동기 및 서버 소켓의 EPOOL 옵션값들을 설정
+ */
 int EpollServer::setOpts()
 {
 	return E_OK;
 }
 
+/**
+ * 소켓 루프 관련 부분
+ */
 int EpollServer::loop()
 {
 	return E_OK;
