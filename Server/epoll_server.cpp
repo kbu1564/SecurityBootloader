3ff91379ccd0aad49b7da94fcb3995f66a00c43b
ByeoungWook Kim <quddnr145@gmail.com>
2015-07-29 14:45:41 +0900

    EpollServer::loop method 구현 & Server EventHandler 클래스 처리 및 추가

--- Server/epoll_server.cpp
+++ Server/epoll_server.cpp
@@ -1,8 +1,10 @@
 #include "include/global.h"
+#include "include/server_handler.h"
 #include "include/epoll_server.h"
 
-EpollServer::EpollServer()
+EpollServer::EpollServer(ServerHandler* handler)
 {
+    this->mHandler = handler;
 }
 
 EpollServer::~EpollServer()
@@ -111,7 +113,7 @@ int EpollServer::listen()
 int EpollServer::_epoll_init(const int epollsize)
 {
 	int retval = 0;
-    int epfd = epoll_create(epollsize);
+    this->mEpfd = epoll_create(epollsize);
     if (epfd == -1) {
         this->__reporting("epoll_create", "can't create epoll events");
 		return -1;
@@ -133,6 +135,77 @@ int EpollServer::_epoll_init(const int epollsize)
  */
 int EpollServer::loop()
 {
+    int retval = 0;
+    epoll_event event, current_event;
+
+    for (;;) {
+        int n = epoll_wait(this->mEpfd, this->mEvents, MAX_EVENTS, -1);
+        for (int i = 0; i < n; i++) {
+            current_event = this->mEvents[i];
+
+            if ((current_event.events & EPOLLERR) ||
+                (current_event.events & EPOLLHUP) ||
+               !(current_event.events & EPOLLIN)) {
+                // epoll error!!
+                this->mHandler->disconnect(current_event);
+            } else if (current_event.events & EPOLLRDHUP) {
+                // closed connection on descriptor via EPOLLRDHUP
+                this->mHandler->disconnect(current_event);
+            } else if (this->mSfd == current_event.data.fd) {
+                // listening socket
+                for (;;) {
+                    sockaddr in_addr;
+                    sockaddr sa;
+                    socklen_t in_len;
+                    int infd;
+                    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+
+                    in_len = sizeof(in_addr);
+                    infd = accept4(this->mSfd, &in_addr, &in_len, SOCK_NONBLOCK, SOCK_CLOEXEC);
+                    if (infd == -1) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                            break;
+                        else {
+                            // accept!!
+                            break;
+                        }
+                    }
+
+                    retval = getnameinfo(&sa, in_len, hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV);
+                    if (retval == 0) {
+                        // host = hbuf, serv = sbuf
+                    }
+
+                    event.data.fd = infd;
+                    event.events  = EPOLLIN | EPOLLRDHUP | EPOLLET;
+                    retval = epoll_ctl(this->mEpfd, EPOLL_CTL_ADD, infd, &event);
+                    if (retval == -1) {
+                        this->__reporting("epoll_ctl", "Register for read events error");
+                        return -1;
+                    }
+
+                    // accept handler
+                    this->accept(event, hbuf, sbuf);
+                }
+            } else if (current_event.events & EPOLLIN) {
+                // read data
+                char buf[MAX_BUFFER_SIZE];
+                int nread = read(current_event.data.fd, buf, MAX_BUFFER_SIZE);
+                if (nread < 1) {
+                    // closed connection on descriptor
+                    this->disconnect(current_event);
+                } else {
+                    // NULL value setting
+                    if (nread < MAX_BUFFER_SIZE) buf[nread] = 0;
+
+                    // recv event!!
+                    this->recv(current_event, buf, nread);
+                }
+            } else if (current_event.events & EPOLLOUT) {
+                // write event!!
+            }
+        }
+    }
     return E_OK;
 }
 
