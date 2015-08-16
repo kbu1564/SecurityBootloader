2d35d39e7a4f7bb10b92601514f95534eb9d319a
ByeoungWook <quddnr145@gmail.com>
2015-08-17 00:51:20 +0900

    test communication to server from grub : hello.mod

--- grub/grub-core/hello/hello.c
+++ grub/grub-core/hello/hello.c
@@ -18,6 +18,10 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <grub/net.h>
+#include <grub/net/arp.h>
+#include <grub/net/tcp.h>
+#include <grub/net/ip.h>
+#include <grub/net/ethernet.h>
 #include <grub/net/netbuff.h>
 #include <grub/time.h>
 #include <grub/file.h>
@@ -27,9 +31,6 @@
 #include <grub/dl.h>
 #include <grub/command.h>
 #include <grub/env.h>
-#include <grub/net/ethernet.h>
-#include <grub/net/arp.h>
-#include <grub/net/ip.h>
 #include <grub/loader.h>
 #include <grub/bufio.h>
 #include <grub/kernel.h>
@@ -42,19 +43,54 @@
 GRUB_MOD_LICENSE ("GPLv3+");
 
 static grub_err_t
+hello_tcp_receive (grub_net_tcp_socket_t sock __attribute__ ((unused)), struct grub_net_buff *nb, void *f __attribute__ ((unused)))
+{
+  grub_err_t err;
+
+  if (!sock)
+  {
+      grub_netbuff_free(nb);
+      return GRUB_ERR_NONE;
+  }
+
+  return err;
+}
+
+static void
+hello_tcp_err (grub_net_tcp_socket_t sock __attribute__ ((unused)), void *f __attribute__ ((unused)))
+{
+}
+
+static grub_err_t
 grub_cmd_hello (grub_extcmd_context_t ctxt __attribute__ ((unused)),
 		int argc __attribute__ ((unused)),
 		char **args __attribute__ ((unused)))
 {
-  grub_printf ("%s\n", _("Get Network Card Lists"));
+  grub_printf ("%s\n", _("Send Network Packet Test"));
 
-  struct grub_net_card *card;
-  FOR_NET_CARDS(card)
+  char server[100];
+  grub_strcpy(server, "119.205.252.21");
+
+  grub_net_tcp_socket_t sock = grub_net_tcp_open(server, 10880, hello_tcp_receive, hello_tcp_err, hello_tcp_err, 0);
+
+  grub_err_t err;
+  struct grub_net_buff *nb = grub_netbuff_alloc(GRUB_NET_TCP_RESERVE_SIZE + grub_strlen("grub -> server test") + 1);
+  if (nb)
   {
-    char buf[GRUB_NET_MAX_STR_HWADDR_LEN];
-    grub_net_hwaddr_to_str (&card->default_address, buf);
-    grub_printf ("%s %s\n", card->name, buf);
+      grub_netbuff_reserve(nb, GRUB_NET_TCP_RESERVE_SIZE);
+      grub_uint8_t *ptr = nb->tail;
+      err = grub_netbuff_put(nb, grub_strlen("grub -> server test") + 1);
+      if (err)
+      {
+          grub_netbuff_free(nb);
+          grub_net_tcp_close(sock, GRUB_NET_TCP_ABORT);
+          return err;
+      }
+      grub_memcpy(ptr, "grub -> server test", grub_strlen("grub -> server test") + 1);
   }
+  err = grub_net_send_tcp_packet(sock, nb, 1);
+  grub_net_tcp_close(sock, GRUB_NET_TCP_ABORT);
+
   return 0;
 }
 
