494fd9bc878fc4241587c8bb353e19554aca9970
yujoo <formfoxk@gmail.com>
2015-08-21 15:29:15 +0900

    #21 Implement PacketExecuteQueue.h

--- Server/include/PacketExecuteQueue.h
+++ Server/include/PacketExecuteQueue.h
@@ -3,20 +3,70 @@
 
 // 원형큐 방식
 // 큐 데이터 배열 크기
-const int QUEUE_SIZE = 100;
 
+template<const int SIZE = 100>
 class PacketExecuteQueue
 {
 private:
-    Packet mPacketData[SIZE];
+    // Member Variable
+    Packet mPacketDataList[SIZE];
     int    mFront;
     int    mRear;
-public:
-    PacketExecuteQueue();
-    ~PacketExecuteQueue();
 
-    void push(const Packet* p);
-    int  popExecute();
+    // Private Function
+    bool isEmpty()
+    {
+        if (mFront == mRear)
+            return true;
+        
+        return false;
+    }
+
+    int getNextIndex(int index)
+    {
+        if (index == SIZE-1)
+            return 0;
+        
+        return index+1;   
+    }
+
+public:
+    PacketExecuteQueue()
+    {
+        this->mFront = 0;
+        this->mRear = 0;
+    }
+
+    ~PacketExecuteQueue() 
+    {
+        delete mPacketDataList;
+    }
+
+    int push(Packet p)
+    {
+        if (getNextIndex(mRear) == mFront)
+            return -1;
+                
+        mRear = getNextIndex(mRear);
+        mPacketDataList[mRear] = p;
+        
+        return 0;
+    }
+    
+    int  popExecute()
+    {
+        // Not exist packet
+        if (isEmpty()) 
+            return -1;
+        
+        // Execute packet execute function
+        Packet p = mPacketDataList[mFront];
+        mFront = getNextIndex(mFront);   
+
+        p.execute(); 
+    
+        return 0;
+    }
 };
 
 #endif
