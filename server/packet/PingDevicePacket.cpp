8f5fe329807ed493f7efd6b22beff960712ae9c2
ByeoungWook <quddnr145@gmail.com>
2015-08-23 23:47:48 +0900

    문자열 체크용 ping 데이터 임시 변경

--- server/packet/PingDevicePacket.cpp
+++ server/packet/PingDevicePacket.cpp
@@ -10,15 +10,17 @@ int PingDevicePacket::parser(const char* buff, const int size)
 
 char* PingDevicePacket::encode(int* size)
 {
-    int ping = 0xFFFF;
+    //int ping = 0xFFFF;
+    char buff[100];
+    sprintf(buff, "Yujoo 류주");
 
     int protocol = PING_DEVICE;
     // 전체 패킷 길이
-    int bufflen = 8 + sizeof(ping);
+    int bufflen = 8 + strlen(buff) + 1;//sizeof(ping);
     char *buff = new char[100];
-    strncpy(buff + 0, (char *)&protocol, 4);
-    strncpy(buff + 0, (char *)&bufflen, 4);
-    strncpy(buff + 0, (char *)&ping, 4);
+    memcpy(buff + 0, (char *)&protocol, 4);
+    memcpy(buff + 4, (char *)&bufflen, 4);
+    memcpy(buff + 8, (char *)&ping, bufflen - 8);
 
     *size = bufflen;
 
