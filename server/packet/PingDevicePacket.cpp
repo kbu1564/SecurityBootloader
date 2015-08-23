ec30c7f995c87b0a82200357fa605804150f2a0f
ByeoungWook <quddnr145@gmail.com>
2015-08-24 00:38:02 +0900

    클라이언트 문자열 엔디안 처리에 따른 테스트 코드 제거

--- server/packet/PingDevicePacket.cpp
+++ server/packet/PingDevicePacket.cpp
@@ -10,13 +10,11 @@ int PingDevicePacket::parser(const char* buff, const int size)
 
 char* PingDevicePacket::encode(int* size)
 {
-    //int ping = 0xFFFF;
-    char buff[100];
-    sprintf(buff, "Yujoo 류주");
+    int ping = 0xFFFF;
 
     int protocol = PING_DEVICE;
     // 전체 패킷 길이
-    int bufflen = 8 + strlen(buff) + 1;//sizeof(ping);
+    int bufflen = 8 + sizeof(ping);
     char *buff = new char[100];
     memcpy(buff + 0, (char *)&protocol, 4);
     memcpy(buff + 4, (char *)&bufflen, 4);
