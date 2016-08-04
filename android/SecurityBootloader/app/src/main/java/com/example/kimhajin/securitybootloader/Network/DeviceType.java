d7e65a3c2f352d3ecea2fe96ff70c5ff270eceaa
poo1130 <yugioh9@naver.com>
2016-08-04 23:04:11 +0900

    Update DeviceType.java

--- android/SecurityBootloader/app/src/main/java/com/example/kimhajin/securitybootloader/Network/DeviceType.java
+++ android/SecurityBootloader/app/src/main/java/com/example/kimhajin/securitybootloader/Network/DeviceType.java
@@ -4,7 +4,6 @@ package com.example.kimhajin.securitybootloader.Network;
  * @FileName		: DeviceType.java 
  * @Project		: SecurityBootloder 
  * @Date			: 2015. 8. 23. 
- * @작성자			: 주현 
  * @프로그램 설명		: Device 장치 타입
  * @프로그램 기능		: 
  * @변경이력		: 
